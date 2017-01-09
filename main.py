790d81fecf4c1a106e828e39dff3d045152ffa8f
vesloguzov <vesloguzov@gmail.com>
2017-01-09 17:56:10 +0500

    sections

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # encoding=utf8
 import sys
+import math
+
 from docx import Document
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+def get_section_margins(section):
+    print "Top: ", round(section.top_margin.cm, 2)
+    print "Bottom: ", round(section.bottom_margin.cm, 2)
+    print "Left: ", round(section.left_margin.cm, 2)
+    print "Right: ", round(section.right_margin.cm, 2)
 
 
-
-
-
+document = Document('data/dummy.docx')
 document = Document('data/internet.docx')
 
 # print "core_properties"
@@ -31,20 +36,28 @@ document = Document('data/internet.docx')
 last_style = ''
 
 for p in document.paragraphs:
+
     if p.style.name != "Normal":
         if "Заголовок" in p.style.name:
-            print '\n'
             print p.style.name, p.text
-            print '\n'
-        elif "Основной" in p.style.name:
-            last_style = p.style.name
-            if last_style == p.style.name:
-                print p.text
-            else:
-                p.style.name, p.text
-                last_style = ''
+        #     print '\n'
+        # elif "Основной" in p.style.name:
+        #     last_style = p.style.name
+        #     if last_style == p.style.name:
+        #         print p.text
+        #     else:
+        #         p.style.name, p.text
+        #         last_style = ''
 
         elif "toc" in p.style.name:
-            print "Оглавление: ", p.text
+            print "Оглавление: ", p.style.name, p.text
         elif "Указатель" in p.style.name:
             print p.style.name, p.text
+
+
+# sections = document.sections
+#
+# for section in sections:
+#     get_section_margins(section)
+#
+# print document.core_properties.modified
