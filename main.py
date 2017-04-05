a4988e9edf50d00082f5db700bd9339ab8cd2901
vesloguzov <vesloguzov@gmail.com>
2017-04-05 20:17:39 +0500

    parser

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import math
 
 from docx import Document
+from docx.shared import Pt
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -35,29 +36,59 @@ document = Document('data/internet.docx')
 # print document.core_properties.version
 last_style = ''
 
-for p in document.paragraphs:
-
-    if p.style.name != "Normal":
-        if "Заголовок" in p.style.name:
-            print p.style.name, p.text
-        #     print '\n'
-        # elif "Основной" in p.style.name:
-        #     last_style = p.style.name
-        #     if last_style == p.style.name:
-        #         print p.text
-        #     else:
-        #         p.style.name, p.text
-        #         last_style = ''
-
-        elif "toc" in p.style.name:
-            print "Оглавление: ", p.style.name, p.text
-        elif "Указатель" in p.style.name:
-            print p.style.name, p.text
-
-
-# sections = document.sections
+# for p in document.paragraphs:
 #
-# for section in sections:
-#     get_section_margins(section)
+#     if p.style.name != "Normal":
+#         if "Заголовок" in p.style.name:
+#             print p.style.name, p.text
+#             print '\n'
+#         elif "Основной" in p.style.name:
+#             last_style = p.style.name
+#             if last_style == p.style.name:
+#                 print p.style.name, p.text
+#             else:
+#                 # p.style.name, p.text
+#                 last_style = ''
 #
-# print document.core_properties.modified
+#         elif "toc" in p.style.name:
+#             print "Оглавление: ", p.style.name, p.text
+#         elif "Указатель" in p.style.name:
+#             print p.style.name, p.text
+
+styles = document.styles
+for style in styles:
+    if "ФИО" in style.name:
+        print style.name
+
+        try:
+            print 'Шрифт: ' + style.font.name
+        except:
+            print 'Родительский стиль: ' + style.base_style.name
+            parentStyle = style.base_style
+            #print 'Шрифт родительского стиля: ' + parentStyle.font.name
+            print 'Ошибка получения имени шрифта'
+            pass
+
+        try:
+            print 'Размер шрифт: ' + style.font.size.pt
+        except:
+            print 'Ошибка получения размера шрифта'
+            pass
+
+        try:
+            paragraph_format = style.paragraph_format
+            print paragraph_format
+            print paragraph_format.first_line_indent.cm
+            print paragraph_format.space_before.pt
+            print paragraph_format.space_after.pt
+            print paragraph_format.line_spacing
+        except:
+            print 'Ошибка получения параметра'
+            pass
+        print '\n'
+
+#style.builtin #Стиль сделан пользователем
+#style.paragraph_format.first_line_indent.cm # Отступ первой строки
+#style.paragraph_format.space_before.pt # Отступ до
+#style.paragraph_format.space_after.pt # Отступ после
+#style.paragraph_format.line_spacing # Межстрочный интервал
