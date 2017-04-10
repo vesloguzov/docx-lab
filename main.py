941aa43875d214f7ec2e18095170f11781e28745
vesloguzov <vesloguzov@gmail.com>
2017-04-10 20:46:42 +0500

    начал

--- main.py
+++ main.py
@@ -10,40 +10,68 @@ from lxml import etree
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-def check_section_margins(sections):
-    top_margin = 0
-    bottom_margin = 0
-    left_margin = 0
-    right_margin = 0
-    for section in sections:
-        print "Top: ", round(section.top_margin.cm, 2)
-        print "Bottom: ", round(section.bottom_margin.cm, 2)
-        print "Left: ", round(section.left_margin.cm, 2)
-        print "Right: ", round(section.right_margin.cm, 2)
+file_analyzes = {}
+
+def get_general_properties(document):
+    general_properties = {}
+    general_properties["author"] = document.core_properties.author # Автор документа
+    general_properties["created"] = document.core_properties.created # Дата создания документа
+    general_properties["last_modified_by"] = document.core_properties.last_modified_by # Пользователь, который менял последний
+    general_properties["modified"] = document.core_properties.modified # Время изменения
+    general_properties["title"] = document.core_properties.title #Название
+    file_analyzes["general_properties"] = general_properties
+
+def get_document_margins(document):
+    margins = {}
+    sections = document.sections
+    margins["top"] = round(sections[0].top_margin.cm, 2)
+    margins["bottom"] = round(sections[0].bottom_margin.cm, 2)
+    margins["left"] = round(sections[0].left_margin.cm, 2)
+    margins["right"] = round(sections[0].right_margin.cm, 2)
+    file_analyzes["margins"] = margins
+
+def get_custom_styles(document):
+    custom_styles = {}
+    all_docx_styles = document.styles
+    for s in all_docx_styles:
+        if s.builtin == False and "ФИО" in s.name:
+            if "Заголовок" in s.name:
+                header_style = {}
+                custom_head_style = all_docx_styles[s.base_style.name]
+                header_style["name"] = s.name
+                header_style["font_name"] = s.font.name
+                header_style["font_size"] = s.font.size
+                header_style["font_italic"] = s.font.italic
+                header_style["font_bold"] = s.font.bold
+                header_style["line_spacing"] = s.paragraph_format.line_spacing
+                header_style["first_line_indent"] = s.paragraph_format.first_line_indent
+                header_style["space_before"] = s.paragraph_format.space_before
+                header_style["space_after"] = s.paragraph_format.space_after
+                header_style["alignment"] = s.paragraph_format.alignment
+                custom_styles["header_style"] = header_style
+            else:
+                paragraph_style = {}
+                custom_paragraph_style = all_docx_styles[s.base_style.name]
+                header_style["name"] = s.name
+                header_style["font_name"] = s.font.name
+
+                print s.paragraph_format.alignment
+
+    file_analyzes["custom_styles"] = custom_styles
+
 
 document = Document('data/internet.docx')
 
-# print "core_properties"
-# print document.core_properties.author
-# print document.core_properties.category
-# print document.core_properties.comments
-# print document.core_properties.content_status
-# print document.core_properties.created
-# print document.core_properties.identifier
-# print document.core_properties.keywords
-# print document.core_properties.language
-# print document.core_properties.last_modified_by
-# print document.core_properties.last_printed
-# print document.core_properties.modified
-# print document.core_properties.revision
-# print document.core_properties.subject
-# print document.core_properties.title
-# print document.core_properties.version
-last_style = ''
+get_general_properties(document)
+get_document_margins(document)
+get_custom_styles(document)
+
+print file_analyzes
 
 
-tables = document.tables
-
+#
+# tables = document.tables
+#
 # for sec in document.sections:
 #     print sec.header.body
 
@@ -56,9 +84,14 @@ tables = document.tables
 # for row in tables[0].rows:
 #     print row.cells[0].paragraphs[0].text + row.cells[2].paragraphs[0].text
 
-for p in document.paragraphs:
+# get_section_margins(document.sections)
+# print  file_analyzes
+
+
+
+# for p in document.paragraphs:
 #     run = p.add_run()
-    print p.text, p.style.paragraph_format.space_after
+#     print p.text, p.style.paragraph_format.space_after
 #     if p.style.name != "Normal":
 #         if "Заголовок" in p.style.name:
 #             print p.style.name, p.text
